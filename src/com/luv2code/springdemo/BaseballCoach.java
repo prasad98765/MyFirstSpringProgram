7fc7a10193dbee28c4aa595ac50399caff41c22f
prasad98765 <pnchaudhari1996@gmail.com>
2020-06-28 12:57:46 +0530

    Added : My First Spring Program Through XML File

---
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach {
+	@Override
+	public String getDeaiyworkout() {
+		return "Spend 30 mintes on batting practice";	
+	}
+}
---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDeaiyworkout() {
+		return "Spend 30 mintes on batting practice on Cricket Srack";
+	}
+
+}
