300edd1007c82d06bcd84180c05bc6cc1b804006
Your Name <you@example.com>
2020-07-02 19:17:34 +0530

    ADDED : Some Spring Program With XML Configuration Without Any Annotation

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		//Create Object
-		
+	
 		Coach theCoach = new TrackCoach();
 		// user Object
 		System.out.println(theCoach.getDeaiyworkout());
